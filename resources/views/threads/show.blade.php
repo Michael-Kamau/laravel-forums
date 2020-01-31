fd07036d216ee23ae775f2e184b71ae1eb36f8ce
Michael-Kamau <kamau.karitu@gmail.com>
2020-01-31 13:31:29 +0300

    A one to many relationship between User and Reply models  and One to many relationship between a Thread and Reply model. Also added a unit test to see whether a thread has a user

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -14,9 +14,20 @@
                             </div>
                         @endif
 
-                            <div class="body">{{$thread->body}}</div>
+                        <div class="body">{{$thread->body}}</div>
                     </div>
                 </div>
+                @foreach($thread ->replies as $reply)
+                    <div class="card">
+                        <div class="card-heading">
+                            <a href="#">{{$reply->owner->name}}</a>
+                             said {{$reply->created_at->diffForHumans()}}
+                        </div>
+                        <div class="card-body">
+                            {{$reply->body }}
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -10,19 +10,18 @@ class ReadThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
-    public function setUp() :void
+    public function setUp(): void
     {
-       parent::setUp();
+        parent::setUp();
 
-       $this->thread=factory('App\Thread')->create();
+        $this->thread = factory('App\Thread')->create();
     }
 
     /** @test */
     public function a_user_can_view_all_threads()
     {
-        $response = $this->get('/threads');
-
-        $response->assertSee($this->thread->title);
+        $response = $this->get('/threads')
+            ->assertSee($this->thread->title);
 
 
     }
@@ -30,17 +29,20 @@ class ReadThreadsTest extends TestCase
     /** @test */
     public function a_user_can_view_a_single_thread()
     {
-        $response = $this->get('/threads/' . $this->thread->id);
+        $response = $this->get('/threads/' . $this->thread->id)
+            ->assertSee($this->thread->title);
 
-        $response->assertSee($this->thread->title);
     }
 
-//    /** @test */
-//    public function a_user_can_read_replies_that_are_associated_with_a_thread()
-//    {
-//        //Given we have a thread
-//        //And the thread includes replies
-//        //we visit
-//    }
+    /** @test */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        //Given we have a thread
+        $reply = factory('App\Reply')
+            ->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/' . $this->thread->id)
+            ->assertSee($reply->body);
+    }
 
 }
