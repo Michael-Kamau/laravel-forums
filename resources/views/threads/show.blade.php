5d2dbf3cbf4942aacc333b2b87b144afc94fde77
Michael-Kamau <kamau.karitu@gmail.com>
2020-01-31 17:33:39 +0300

    (Episode 5)Created a reply form for the logged in users to reply to threads

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -20,6 +20,23 @@
                 @foreach($thread ->replies as $reply)
                     @include('threads.reply')
                 @endforeach
+                @if(auth()->check())
+                    <div class="row">
+                        <div class="col-md-12 col-md-offset-2">
+                            <form method="POST" action="{{$thread->path().'/replies'}}">
+                                {{csrf_field()}}
+                                <div class="form-group">
+                                    <label for="exampleFormControlTextarea1">Body:</label>
+                                    <textarea name="body" class="form-control" id="body" rows="5"
+                                              placeholder="Have Something to say?"></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-secondary">Success</button>
+                            </form>
+                        </div>
+                    </div>
+                @else
+                    <p class="text-center"> Please <a href="{{route('login')}}">Sign in</a> to participate in this discussion</p>
+                @endif
             </div>
         </div>
     </div>
