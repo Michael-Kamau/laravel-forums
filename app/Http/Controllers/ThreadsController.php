e0fe6170dadb5b88b4a01932b007b1ade396482a
Michael-Kamau <kamau.karitu@gmail.com>
2020-02-02 01:06:16 +0300

    (Episode 15) Refactoring the code using filters

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -2,41 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Channel;
+use App\Filters\ThreadFilters;
 use App\Thread;
 use Illuminate\Http\Request;
-use App\Channel;
 
 class ThreadsController extends Controller
 {
-
+    /**
+     * ThreadsController constructor.
+     */
     public function __construct()
     {
-        $this->middleware('auth')->except(['index','show']);
+        $this->middleware('auth')->except(['index', 'show']);
     }
+
 
     /**
      * Display a listing of the resource.
      *
-     * @param Channel $channel
+     * @param  Channel      $channel
+     * @param ThreadFilters $filters
      * @return \Illuminate\Http\Response
      */
-    public function index(Channel $channel)
+    public function index(Channel $channel, ThreadFilters $filters)
     {
-        //
-        if ($channel->exists) {
-            $threads = $channel->threads()->latest();
-        } else {
-            $threads = Thread::latest();
-        }
-
-        // if request('by'), we should filter by the given username.
-        if ($username = request('by')) {
-            $user = \App\User::where('name', $username)->firstOrFail();
-
-            $threads->where('user_id', $user->id);
-        }
-
-        $threads = $threads->get();
+        $threads = $this->getThreads($channel, $filters);
 
         return view('threads.index', compact('threads'));
     }
@@ -48,23 +39,23 @@ class ThreadsController extends Controller
      */
     public function create()
     {
-        //
         return view('threads.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        $this->validate($request,[
-            'title'=>'required',
-            'body'=>'required',
-            'channel_id'=>'required|exists:channels,id'
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required',
+            'channel_id' => 'required|exists:channels,id'
         ]);
+
         $thread = Thread::create([
             'user_id' => auth()->id(),
             'channel_id' => request('channel_id'),
@@ -73,52 +64,35 @@ class ThreadsController extends Controller
         ]);
 
         return redirect($thread->path());
-
     }
 
     /**
      * Display the specified resource.
      *
-     * @param $channelId
-     * @param \App\Thread $thread
+     * @param  integer     $channelId
+     * @param  \App\Thread $thread
      * @return \Illuminate\Http\Response
      */
-    public function show($channelId,Thread $thread)
+    public function show($channelId, Thread $thread)
     {
         return view('threads.show', compact('thread'));
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Fetch all relevant threads.
      *
-     * @param \App\Thread $thread
-     * @return \Illuminate\Http\Response
+     * @param Channel       $channel
+     * @param ThreadFilters $filters
+     * @return mixed
      */
-    public function edit(Thread $thread)
+    protected function getThreads(Channel $channel, ThreadFilters $filters)
     {
-        //
-    }
+        $threads = Thread::latest()->filter($filters);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Thread $thread
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Thread $thread)
-    {
-        //
-    }
+        if ($channel->exists) {
+            $threads->where('channel_id', $channel->id);
+        }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Thread $thread
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Thread $thread)
-    {
-        //
+        return $threads->get();
     }
 }
