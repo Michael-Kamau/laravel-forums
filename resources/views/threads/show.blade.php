f9dfd5f71a9d5355ca23e7e1f0fadba2fea66ab2
Michael-Kamau <kamau.karitu@gmail.com>
2020-02-02 15:26:58 +0300

    (Episode 16) Meta details and pagination

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,41 +2,52 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header"><a href="#">{{$thread->creator->name}}</a> posted: {{$thread->title}}</div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <a href="#">{{ $thread->creator->name }}</a> posted:
+                        {{ $thread->title }}
+                    </div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        <div class="body">{{$thread->body}}</div>
+                    <div class="panel-body">
+                        {{ $thread->body }}
                     </div>
                 </div>
-                @foreach($thread ->replies as $reply)
-                    @include('threads.reply')
+
+                @foreach ($replies as $reply)
+                    @include ('threads.reply')
                 @endforeach
-                @if(auth()->check())
-                    <div class="row">
-                        <div class="col-md-12 col-md-offset-2">
-                            <form method="POST" action="{{$thread->path().'/replies'}}">
-                                {{csrf_field()}}
-                                <div class="form-group">
-                                    <label for="exampleFormControlTextarea1">Body:</label>
-                                    <textarea name="body" class="form-control" id="body" rows="5"
-                                              placeholder="Have Something to say?"></textarea>
-                                </div>
-                                <button type="submit" class="btn btn-secondary">Success</button>
-                            </form>
+
+                {{ $replies->links() }}
+
+                @if (auth()->check())
+                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <textarea name="body" id="body" class="form-control" placeholder="Have something to say?"
+                                      rows="5"></textarea>
                         </div>
-                    </div>
+
+                        <button type="submit" class="btn btn-default">Post</button>
+                    </form>
                 @else
-                    <p class="text-center"> Please <a href="{{route('login')}}">Sign in</a> to participate in this discussion</p>
+                    <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this
+                        discussion.</p>
                 @endif
+            </div>
+
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p>
+                            This thread was published {{ $thread->created_at->diffForHumans() }} by
+                            <a href="#">{{ $thread->creator->name }}</a>, and currently
+                            has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
