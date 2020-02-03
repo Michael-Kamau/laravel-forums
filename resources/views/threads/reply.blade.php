11696e4f8d98e6898ac7bd398ee94f3f403f7f4d
Michael-Kamau <kamau.karitu@gmail.com>
2020-02-03 16:00:13 +0300

    (Episode 32) A vue component for reply with edit reply

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,8 @@
 <reply :attributes="{{ $reply }}" inline-template v-cloak>
-    <div id="reply-{{ $reply->id }}" class="panel panel-default">
+    <div id="reply-{{ $reply->id }}" class="panel panel-default card">
         <div class="panel-heading">
-            <div class="level">
-                <h5 class="flex">
+            <div class="level card-header">
+                <h5 class="flex ">
                     <a href="{{ route('profile', $reply->owner) }}">
                         {{ $reply->owner->name }}
                     </a> said {{ $reply->created_at->diffForHumans() }}...
@@ -30,12 +30,12 @@
                 <button class="btn btn-xs btn-link" @click="editing = false">Cancel</button>
             </div>
 
-            <div v-else v-text="body"></div>
+            <div v-else v-text="body" class="card-body"></div>
         </div>
 
         @can ('update', $reply)
             <div class="panel-footer level">
-                <button class="btn btn-xs mr-1" @click="editing = true">Edit</button>
+                <button class="btn btn-info" @click="editing = true">Edit</button>
 
                 <form method="POST" action="/replies/{{ $reply->id }}">
                     {{ csrf_field() }}
@@ -47,3 +47,61 @@
         @endcan
     </div>
 </reply>
+
+
+
+
+
+
+
+
+
+
+
+
+{{--<reply inline-template>--}}
+{{--    <div id="reply-{{ $reply->id }}" class="panel panel-default">--}}
+{{--        <div class="panel-heading">--}}
+{{--            <div class="level">--}}
+{{--                <h5 class="flex">--}}
+{{--                    <a href="{{ route('profile', $reply->owner) }}">--}}
+{{--                        {{ $reply->owner->name }}--}}
+{{--                    </a> said {{ $reply->created_at->diffForHumans() }}...--}}
+{{--                </h5>--}}
+
+{{--                <div>--}}
+{{--                    <form method="POST" action="/replies/{{ $reply->id }}/favorites">--}}
+{{--                        {{ csrf_field() }}--}}
+{{--                        2--}}
+{{--                        <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>--}}
+{{--                            {{ $reply->favorites_count }} {{ str_plural('Favorite', $reply->favorites_count) }}--}}
+{{--                        </button>--}}
+{{--                    </form>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+
+{{--        <div class="panel-body">--}}
+{{--            {{ $reply->body }}--}}
+{{--        </div>--}}
+
+{{--        @can ('update', $reply)--}}
+{{--            <div class="panel-footer level mr-1">--}}
+{{--                <button class="btn btn-success btn-xs">--}}
+{{--                    EDIT--}}
+{{--                </button>--}}
+{{--                <form method="POST" action="/replies/{{ $reply->id }}">--}}
+{{--                    {{ csrf_field() }}--}}
+{{--                    {{ method_field('DELETE') }}--}}
+
+{{--                    <button type="submit" class="btn btn-danger btn-xs">Delete</button>--}}
+{{--                </form>--}}
+{{--            </div>--}}
+{{--        @endcan--}}
+
+{{--    </div>--}}
+{{--</reply>--}}
+
+
+
+
