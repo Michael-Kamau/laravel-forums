19c0b6b1c1cf38f8804bfe7ed4812b8d8eeb024a
Michael-Kamau <kamau.karitu@gmail.com>
2020-02-15 23:17:56 +0300

    Handling replies

---
+++ resources/js/components/Replies.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <dv v-for="reply in items">
+        <reply :data="reply"></reply>
+    </dv>
+</div>
+</template>
+
+<script>
+    import Reply from "./Reply.vue";
+    export default {
+        name: "Replies.vue",
+        props:['data'],
+        data(){
+            return{
+                items:this.data
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
